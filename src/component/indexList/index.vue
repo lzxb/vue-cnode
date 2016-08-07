b629c04f3b9f9858f2acfaf5a65b7eacc3c9e7ec
狼族小狈 <1340641314@qq.com>
2016-08-07 23:12:38 +0800

    置顶和精华图标

--- src/component/indexList/index.vue
+++ src/component/indexList/index.vue
@@ -1,6 +1,21 @@
+<style lang="less" scoped rel="stylesheet/less">
+    @import "./index";
+</style>
+<script>
+    import index from './index'
+    export default index
+</script>
 <template>
     <ul class="list" v-cloak>
         <li v-for="o in data">
+            <div class="typeicon" flex v-if="o.top || o.good">
+                <div class="icon" v-if="o.top">
+                    <i class="iconfont icon-zhiding"></i>
+                </div>
+                <div class="icon" v-if="o.good">
+                    <i class="iconfont icon-jinghua"></i>
+                </div>
+            </div>
             <div class="user" flex="box:first">
                 <div class="headimg">
                     <div class="pic">
@@ -20,33 +35,23 @@
                     </div>
                 </div>
             </div>
-            <div class="expand" flex="box:last">
-                <div class="count" flex>
-                    <div class="icon">
-                        <i class="iconfont icon-chakan"></i>
-                        <div class="tit">{{o.visit_count}}</div>
-                    </div>
-                    <div class="count">
-                        <i class="iconfont icon-pinglun"></i>
-                        <div class="tit">{{o.reply_count}}</div>
-                    </div>
+            <div class="expand" flex="box:mean">
+                <div class="item click" flex="main:center cross:center">
+                    <i class="iconfont icon-chakan"></i>
+                    <div class="num">{{o.visit_count}}</div>
                 </div>
-                <div class="last-reply">
+                <div class="item reply" flex="main:center cross:center">
+                    <i class="iconfont icon-pinglun"></i>
+                    <div class="num">{{o.reply_count}}</div>
+                </div>
+                <!--<div class="item last-reply" flex>
                     <div class="pic">
                         <img :src="transparent" alt="">
                     </div>
                     <div class="name">{{o.author.loginname}}</div>
                     <time class="time">{{o.last_reply_at | formatDate}}</time>
-                </div>
+                </div>-->
             </div>
         </li>
     </ul>
 </template>
-<script>
-    import index from './index'
-    export default index
-</script>
-
-<style lang="less" scoped rel="stylesheet/less">
-    @import "./index";
-</style>
