2629cf49401169527d28b74cb005eedefef13c0b
狼族小狈 <1340641314@qq.com>
2016-08-24 11:23:23 +0800

    优化用户资料

--- src/component/userView.vue
+++ src/component/userView.vue
@@ -170,10 +170,9 @@
         },
         route: {
             data({from}) {
-
-                if(from.title != '主题详情')
-
-                this.SET_CUSTOM_KEY({tabIndex: 0}) //切换访问的用户时，重新将选项卡设置为回复
+                if (from.title != '主题详情') {
+                    this.SET_CUSTOM_KEY({tabIndex: 0})
+                }
                 var {loginname} = this.$route.params
                 Tool.get(`/api/v1/user/${loginname}`, {}, ({data}) => {
                     if(data) {
