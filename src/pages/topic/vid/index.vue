38dde4f074b257c8d08e7cf973a48ddbdf73b98e
狼族小狈 <1340641314@qq.com>
2016-11-07 00:11:55 +0800

    主题详情

--- src/pages/topic/vid/index.vue
+++ src/pages/topic/vid/index.vue
@@ -3,7 +3,11 @@
 </style>
 <template>
     <div>
-        <v-header title="主题"></v-header>
+        <v-header title="主题">
+            <div slot="left" class="item" flex="main:center cross:center" v-on:click="$router.go(-1)">
+                <i class="iconfont icon-back"></i>
+            </div>
+        </v-header>
         <v-content style="bottom: 0;">
             内容详情
         </v-content>
