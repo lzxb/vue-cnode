d10ffcae8f33805e4ffa6dfcb7e5a17ec8cf90b4
狼族小狈 <1340641314@qq.com>
2016-12-04 22:17:26 +0800

    增加启动图

--- src/app.vue
+++ src/app.vue
@@ -8,6 +8,19 @@
     export default {
         data() {
             return {}
+        },
+        mounted() {
+            this.appShow()
+        },
+        methods: {
+            appShow() {
+                const END_TIME = new Date().getTime() //结束时间
+                const diffTime = END_TIME - PAGE_START_TIME 
+                const timer = setTimeout(() => {
+                    clearTimeout(timer)
+                    document.querySelector('.app-loading').className += ' app-loading-hide'
+                }, diffTime > 2000 ? 0 : 2000 - diffTime)
+            }
         }
     }
 </script>
