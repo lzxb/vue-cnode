2e34d8fb55173b36524a13c98b28e0abe5c9c8b8
狼族小狈 <1340641314@qq.com>
2017-01-04 11:31:43 +0800

    永久存储

--- src/store/user.js
+++ src/store/user.js
@@ -4,14 +4,14 @@ export const USER_SIGNIN = 'USER_SIGNIN' //登录成功
 export const USER_SIGNOUT = 'USER_SIGNOUT' //退出登录
 
 export default {
-    state: JSON.parse(sessionStorage.getItem('user')) || {},
+    state: JSON.parse(localStorage.getItem('user')) || {},
     mutations: {
         [USER_SIGNIN](state, user) {
-            sessionStorage.setItem('user', JSON.stringify(user))
+            localStorage.setItem('user', JSON.stringify(user))
             Object.assign(state, user)
         },
         [USER_SIGNOUT](state) {
-            sessionStorage.removeItem('user')
+            localStorage.removeItem('user')
             Object.keys(state).forEach(k => Vue.delete(state, k))
         }
     },
