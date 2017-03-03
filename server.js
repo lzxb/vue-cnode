a7c06c8b21a142deb8111b3a9e1e5bca3f7e7375
狼族小狈 <1340641314@qq.com>
2017-03-04 03:54:36 +0800

    update:webpack

--- server.js
+++ server.js
@@ -1,24 +1,18 @@
-const Webpack = require('webpack')
-const WebpackDevServer = require('webpack-dev-server')
+const express = require('express')
+const webpack = require('webpack')
 const webpackConfig = require('./webpack.config')
-const config = require('./config/')
 
-var compiler = Webpack(webpackConfig)
-var server = new WebpackDevServer(compiler, {
-    publicPath: config.publicPath,
-    stats: {
-        colors: true //显示不同的颜色区分打包的文件
-    }
+const app = express()
+const compiler = webpack(webpackConfig)
+
+var devMiddleware = require('webpack-dev-middleware')(compiler, {
+  publicPath: webpackConfig.output.publicPath,
+  stats: 'normal'
 })
 
-server.app.get(`${config.base}*`, function (req, res) {
-    res.sendFile(__dirname + '/index.html')
-})
+app.use(devMiddleware)
 
-server.listen(3000, (err) => {
-    if (err) {
-        console.log(err)
-        return
-    }
-    console.log('http://localhost:3000' + config.base)
+app.listen(3000, (err) => {
+  if (err) return console.log(err)
+  console.log('http://localhost:3000/')
 })
--- webpack.config.js
+++ webpack.config.js
@@ -1,90 +1,107 @@
 const path = require('path')
-const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const config = require('./config/')
-const IS_ENV = process.env.NODE_ENV == 'production'
-
-
-var plugins = []
-if (IS_ENV) { //生产环境
-    plugins.push(new webpack.DefinePlugin({
-        'process.env': { //设置成生产环境
-            NODE_ENV: JSON.stringify('production')
-        }
-    }))
-    plugins.push(new webpack.optimize.UglifyJsPlugin({ //压缩代码
-        compress: {
-            warnings: false
-        }
-    }))
-}
-
-plugins.push(
-    new HtmlWebpackPlugin({ //根据模板插入css/js等生成最终HTML
-        filename: '../index.html', //生成的html存放路径，相对于 path
-        template: './src/template/index.html', //html模板路径
-    })
-)
-
 
 module.exports = {
-    entry: ['./src/main.js'], //编译入口文件
-    output: {
-        publicPath: config.publicPath, //服务器的路径
-        path: path.resolve(__dirname + config.publicPath), //编译到指定目录下
-        filename: '[name].js?v=[hash]' //编译后的文件名
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js(x)*$/,
-                exclude: /^node_modules$/,
-                loader: 'babel'
-            },
-            {
-                test: /\.vue$/,
-                loader: 'vue'
-            },
-            {
-                test: /\.css/,
-                exclude: /^node_modules$/,
-                loader: `style-loader!css-loader!autoprefixer-loader?{ browsers: ['last 100 versions'] }!`
-            },
-            {
-                test: /\.less/,
-                exclude: /^node_modules$/,
-                loader: `style-loader!css-loader!autoprefixer-loader?{ browsers: ['last 100 versions'] }!less-loader`
-            },
-            {
-                test: /\.(png|jpg)$/,
-                exclude: /^node_modules$/,
-                loader: 'url?limit=2000&name=[name].[ext]?v=[hash]' //注意后面那个limit的参数，当你图片大小小于这个限制的时候，会自动启用base64编码图片
-            },
-            {
-                test: /\.(eot|woff|svg|ttf|woff2|gif|appcache)(\?|$)/,
-                exclude: /^node_modules$/,
-                loader: 'file-loader?name=[name].[ext]?v=[hash]'
+  target: 'web',
+  entry: {
+    main: './src/main.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/vue-cnode/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.vue$/,
+        use: [
+          {
+            loader: 'vue-loader',
+            options: {
+              postcss: [
+                require('autoprefixer')({
+                  browsers: ['iOS >= 7', 'Android >= 4.1']
+                })
+              ]
             }
+          }
         ]
-    },
-    plugins,
-    resolve: {
-        extensions: ['', '.js', '.vue', '.jsx'], //后缀名自动补全
-        alias: {
-            vue: 'vue/dist/vue.js', //webpack打包时，需要设置别名
-            config: path.resolve('src/config/config.js'), //程序的一些基本配置
-            store: path.resolve('src/store/'), //常用工具方法
-            util: path.resolve('src/util/index.js'), //常用工具方法
-            'is-seeing': path.resolve('src/util/is-seeing.js'),
-            'pull-list': path.resolve('src/mixins/pull-list.js'), //拉取列表
-            'route-data': path.resolve('src/lib/route-data/index.js'), //组件数据缓存
-        }
-    },
-    vue: {
-        postcss: [
-            require('autoprefixer')({
-                browsers: ['last 100 versions']
-            })
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader', 
+          'css-loader', 
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins () {
+                return [
+                  require('autoprefixer')({
+                    browsers: ['iOS >= 7', 'Android >= 4.1']
+                  })
+                ]
+              }
+            }
+          }
         ]
-    }
+      }, 
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader', 
+          'css-loader', 
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins () {
+                return [
+                  require('autoprefixer')({
+                    browsers: ['iOS >= 7', 'Android >= 4.1']
+                  })
+                ]
+              }
+            }
+          },
+          'less-loader'
+        ]
+      }, 
+      {
+        test: /\.(eot|woff|svg|ttf|woff2|gif|appcache)(\?|$)/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(png|jpg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 2000
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src/template/index.html') })
+  ],
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      'config': path.resolve(__dirname, 'src/config/config.js'), //程序的一些基本配置
+      'store': path.resolve(__dirname, 'src/store/'), //常用工具方法
+      'util': path.resolve(__dirname, 'src/util/index.js'), //常用工具方法
+      'is-seeing': path.resolve(__dirname, 'src/util/is-seeing.js'),
+      'pull-list': path.resolve(__dirname, 'src/mixins/pull-list.js'), //拉取列表
+      'route-data': path.resolve(__dirname, 'src/lib/route-data/index.js') //页面数据缓存
+    },
+    extensions: ['.js', '.vue', '.json']
+  },
+  devtool: '#cheap-module-eval-source-map'
 }
