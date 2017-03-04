4bba185aae33748cea6332ef1a52ed6c344e7598
狼族小狈 <1340641314@qq.com>
2017-03-05 00:22:14 +0800

    fix:target

--- configs/base.js
+++ configs/base.js
@@ -1,5 +1,5 @@
 module.exports = {
-  target: 'http://www.vue-js.com/', // api请求的目标网站
+  target: 'https://cnodejs.org/', // api请求的目标网站
   base: '/vue-cnode/', // 路由根路径
   publicPath: '/vue-cnode/dist/', // 程序文件在服务器所在的路径
   title: 'vue-cnode 中国最专业的 Node.js 开源技术社区'
