acbdb9809130a50a6da0e41be4bad852d6fe70b6
lzxb <1340641314@qq.com>
2017-06-14 22:09:18 +0800

    fix:index component

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,3 +1,78 @@
+<template>
+  <div>
+    <nav class="nav">
+      <ul flex="box:mean">
+        <li :class="{ active: !this.$route.query.tab }">
+          <router-link to="/">全部</router-link>
+        </li>
+        <li :class="{ active: this.$route.query.tab == 'good' }">
+          <router-link to="/?tab=good">精华</router-link>
+        </li>
+        <li :class="{ active: this.$route.query.tab == 'share' }">
+          <router-link to="/?tab=share">分享</router-link>
+        </li>
+        <li :class="{ active: this.$route.query.tab == 'ask' }">
+          <router-link to="/?tab=ask">问答</router-link>
+        </li>
+        <li :class="{ active: this.$route.query.tab == 'job' }">
+          <router-link to="/?tab=job">招聘</router-link>
+        </li>
+      </ul>
+    </nav>
+    <v-content v-route-scroll="{ path: 'topic-list', name: 'list' }">
+      <ul class="list">
+        <li v-for="item in list.data" key="item.id">
+          <router-link :to="'/topic/' + item.id">
+            <div class="top" flex="box:first">
+              <div class="headimg" :style="{ backgroundImage: 'url(' + item.author.avatar_url + ')' }"></div>
+              <div class="box" flex="dir:top">
+                <strong>{{ item.author.loginname }}</strong>
+                <div flex>
+                  <time>{{ item.create_at | formatDate }}</time>
+                  <span class="tag">#分享#</span>
+                </div>
+              </div>
+            </div>
+            <div class="common-typeicon" flex v-if="item.top || item.good">
+              <div class="icon" v-if="item.good">
+                <i class="iconfont icon-topic-good"></i>
+              </div>
+              <div class="icon" v-if="item.top">
+                <i class="iconfont icon-topic-top"></i>
+              </div>
+            </div>
+            <div class="tit">{{ item.title }}</div>
+            <div class="expand" flex="box:mean">
+              <div class="item click" flex="main:center cross:center">
+                <i class="iconfont icon-click"></i>
+                <div class="num">{{ item.visit_count > 0 ? item.visit_count : '暂无阅读' }}</div>
+              </div>
+              <div class="item reply" flex="main:center cross:center">
+                <i class="iconfont icon-comment"></i>
+                <div class="num">{{ item.reply_count > 0 ? item.reply_count : '暂无评论' }}</div>
+              </div>
+              <div class="item last-reply" flex="main:center cross:center">
+                <time class="time">{{ item.last_reply_at | formatDate }}</time>
+              </div>
+            </div>
+          </router-link>
+        </li>
+      </ul>
+      <v-loading :done="list.done" :loading="list.loading" @seeing="$vuet.fetch('topic-list', { pull: true })"></v-loading>
+    </v-content>
+    <v-footer></v-footer>
+  </div>
+</template>
+<script>
+  import { mapModules, mapRules } from 'vuet'
+
+  export default {
+    mixins: [
+      mapModules({ list: 'topic-list' }),
+      mapRules({ route: 'topic-list' })
+    ]
+  }
+</script>
 <style lang="less" scoped>
   @import "../../less/config";
   .nav {
@@ -135,91 +210,3 @@
     }
   }
 </style>
-<template>
-  <div>
-    <nav class="nav">
-      <ul flex="box:mean">
-        <li :class="{ active: !this.$route.query.tab }">
-          <router-link to="/">全部</router-link>
-        </li>
-        <li :class="{ active: this.$route.query.tab == 'good' }">
-          <router-link to="/?tab=good">精华</router-link>
-        </li>
-        <li :class="{ active: this.$route.query.tab == 'share' }">
-          <router-link to="/?tab=share">分享</router-link>
-        </li>
-        <li :class="{ active: this.$route.query.tab == 'ask' }">
-          <router-link to="/?tab=ask">问答</router-link>
-        </li>
-        <li :class="{ active: this.$route.query.tab == 'job' }">
-          <router-link to="/?tab=job">招聘</router-link>
-        </li>
-      </ul>
-    </nav>
-    <v-content v-route-scroll="{ path: 'topic-list', name: 'list' }">
-      <ul class="list">
-        <li v-for="item in list.data" key="item.id">
-          <router-link :to="'/topic/' + item.id">
-            <div class="top" flex="box:first">
-              <div class="headimg" :style="{ backgroundImage: 'url(' + item.author.avatar_url + ')' }"></div>
-              <div class="box" flex="dir:top">
-                <strong>{{ item.author.loginname }}</strong>
-                <div flex>
-                  <time>{{ item.create_at | formatDate }}</time>
-                  <span class="tag">#分享#</span>
-                </div>
-              </div>
-            </div>
-            <div class="common-typeicon" flex v-if="item.top || item.good">
-              <div class="icon" v-if="item.good">
-                <i class="iconfont icon-topic-good"></i>
-              </div>
-              <div class="icon" v-if="item.top">
-                <i class="iconfont icon-topic-top"></i>
-              </div>
-            </div>
-            <div class="tit">{{ item.title }}</div>
-            <div class="expand" flex="box:mean">
-              <div class="item click" flex="main:center cross:center">
-                <i class="iconfont icon-click"></i>
-                <div class="num">{{ item.visit_count > 0 ? item.visit_count : '暂无阅读' }}</div>
-              </div>
-              <div class="item reply" flex="main:center cross:center">
-                <i class="iconfont icon-comment"></i>
-                <div class="num">{{ item.reply_count > 0 ? item.reply_count : '暂无评论' }}</div>
-              </div>
-              <div class="item last-reply" flex="main:center cross:center">
-                <time class="time">{{ item.last_reply_at | formatDate }}</time>
-              </div>
-            </div>
-          </router-link>
-        </li>
-      </ul>
-      <v-loading :done="list.done" :loading="list.loading" @seeing="$vuet.fetch('topic-list', { pull: true })"></v-loading>
-    </v-content>
-    <v-footer></v-footer>
-  </div>
-</template>
-<script>
-  import { mapModules, mapRules } from 'vuet'
-  // import pullList from 'pull-list'
-
-  export default {
-    mixins: [
-      // pullList,
-      mapModules({ list: 'topic-list' }),
-      mapRules({ route: 'topic-list' })
-    ],
-    methods: {
-      _pullList () {
-        var { page, $route } = this
-        var { tab = 'all' } = $route.query
-        return {
-          url: 'topics',
-          data: { page, tab }
-        }
-      }
-    }
-  }
-
-</script>
